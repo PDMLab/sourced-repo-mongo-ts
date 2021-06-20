b24863d90d689cc77f498127a37d18634fa82ec6
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2021-06-20 23:10:07 +0200

    refactor: use `async/await` instead of callbacks

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,34 @@
-import { Db } from 'mongodb'
+import { Collection, Db, ObjectID } from 'mongodb'
 import logger from 'debug'
 const log = logger('sourced-repo-mongo')
 import _ from 'lodash'
+import { SourcedEntity } from 'sourced-ts'
+
+type Snapshot = SourcedEntity & {
+  _id: ObjectID
+}
+
+type Entity = SourcedEntity & {
+  id: string
+}
 
 export class Repository {
-  entityType: any
+  entityType: (snapshot, events) => void
   indices: string[]
   snapshotFrequency: number
-  snapshots: any
-  events: any
+  snapshots: Collection<any>
+  events: Collection<any>
   constructor(
     entityType,
     options: { db: Db; indices?: string[]; snapshotFrequency?: number }
   ) {
     options = options || { db: null }
-    // EventEmitter.call(this);
     if (!options.db) {
       throw new Error(
         "mongo has not been initialized. you must call require('sourced-repo-mongo/mongo').connect(config.MONGO_URL); before instantiating a Repository"
       )
     }
     const indices = _.union(options.indices, ['id'])
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    // const self = this
     const db = options.db
     this.entityType = entityType
     this.indices = indices
@@ -37,7 +43,6 @@ export class Repository {
   }
 
   async init(): Promise<void> {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
     this.indices.forEach(async (index) => {
       await this.snapshots.createIndex(index)
       await this.events.createIndex(index)
@@ -45,7 +50,6 @@ export class Repository {
     this.events
       .createIndex({ id: 1, version: 1 }, { unique: true, background: false })
       .then(async (index) => {
-        console.log('index created', index)
         await this.snapshots.createIndex(
           { id: 1, version: 1 },
           { unique: true, background: false }
@@ -56,58 +60,41 @@ export class Repository {
     log('initialized %s entity store', this.entityType.name)
   }
 
-  commit(entity, options?, cb?) {
-    if (typeof options === 'function') {
-      cb = options
+  async commit(entity, options?): Promise<void> {
+    if (!options) {
       options = {}
     }
 
     log('committing %s for id %s', this.entityType.name, entity.id)
 
-    this._commitEvents(entity, (err) => {
-      if (err) return cb(err)
+    await this._commitEvents(entity)
 
-      this._commitSnapshots(entity, options, (err) => {
-        if (err) return cb(err)
-        this._emitEvents(entity)
-        return cb()
-      })
-    })
+    await this._commitSnapshots(entity, options)
+    await this._emitEvents(entity)
   }
 
-  commitAll(entities, options?, cb?) {
-    if (typeof options === 'function') {
-      cb = options
+  async commitAll(entities: Entity[], options?): Promise<void> {
+    if (!options) {
       options = {}
     }
 
     log('committing %s for id %j', this.entityType.name, _.map(entities, 'id'))
 
-    this._commitAllEvents(entities, (err) => {
-      if (err) return cb(err)
-      this._commitAllSnapshots(entities, options, (err) => {
-        if (err) return cb(err)
-        entities.forEach((entity) => {
-          this._emitEvents(entity)
-        })
-        return cb()
-      })
+    await this._commitAllEvents(entities)
+    await this._commitAllSnapshots(entities, options)
+    entities.forEach((entity) => {
+      this._emitEvents(entity)
     })
   }
 
-  get(id, cb) {
-    return this._getByIndex('id', id, cb)
+  async get(id): Promise<any> {
+    return this._getByIndex('id', id)
   }
 
-  _getByIndex(index, value, cb) {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const self = this
-
+  async _getByIndex(index, value): Promise<any> {
     if (this.indices.indexOf(index) === -1)
       throw new Error(
         `Cannot get sourced entity type ${this.entityType} by index ${index}`
-        // this.entityType,
-        // index
       )
 
     log('getting %s for %s %s', this.entityType.name, index, value)
@@ -115,64 +102,48 @@ export class Repository {
     const criteria: { version?: any; $or?: any } = {}
     criteria[index] = value
 
-    this.snapshots
+    const snapshots = await this.snapshots
       .find(criteria)
       .sort({ version: -1 })
       .limit(-1)
-      .toArray((err, snapshots) => {
-        if (err) return cb(err)
+      .toArray()
 
-        const snapshot = snapshots[0]
+    const snapshot = snapshots[0]
 
-        if (snapshot) criteria.version = { $gt: snapshot.version }
-        self.events
-          .find(criteria)
-          .sort({ version: 1 })
-          .toArray((err, events) => {
-            if (err) return cb(err)
-            if (snapshot) delete snapshot._id
-            if (!snapshot && !events.length) return cb(null, null)
+    if (snapshot) criteria.version = { $gt: snapshot.version }
+    const events = await this.events
+      .find(criteria)
+      .sort({ version: 1 })
+      .toArray()
+    if (snapshot) delete snapshot._id
+    if (!snapshot && !events.length) return
 
-            const id =
-              index === 'id' ? value : snapshot ? snapshot.id : events[0].id
+    const id = index === 'id' ? value : snapshot ? snapshot.id : events[0].id
 
-            const entity = self._deserialize(id, snapshot, events)
-            return cb(null, entity)
-          })
-      })
+    const entity = this._deserialize(id, snapshot, events)
+    return entity
   }
 
-  getAll(ids?, cb?) {
-    if (typeof ids === 'function') {
-      cb = ids
-      ids = null
-      return this.events.distinct('id', (err, distinctEventIds) => {
-        this.getAll(distinctEventIds, cb)
-      })
+  async getAll(ids?: string[]): Promise<Entity[]> {
+    if (!ids) {
+      const distinctEventIds = await this.events.distinct('id')
+      return await this.getAll(distinctEventIds)
     }
 
     log('getting %ss for ids %j', this.entityType.name, ids)
 
-    if (ids.length === 0) return cb(null, [])
-
-    this._getAllSnapshots(ids, (err, snapshots) => {
-      if (err) return cb(err)
-      this._getAllEvents(ids, snapshots, (err, entities) => {
-        if (err) return cb(err)
-        return cb(null, entities)
-      })
-    })
+    if (ids.length === 0) return []
+    const snapshots = await this._getAllSnapshots(ids)
+    const entities = await this._getAllEvents(ids, snapshots)
+    return entities
   }
 
-  _commitEvents(entity, cb) {
-    if (entity.newEvents.length === 0) return cb()
+  async _commitEvents(entity) {
+    if (entity.newEvents.length === 0) return
 
     if (!entity.id)
-      return cb(
-        new Error(
-          `Cannot commit an entity of type ${this.entityType} without an [id] property`
-          // this.entityType
-        )
+      throw new Error(
+        `Cannot commit an entity of type ${this.entityType} without an [id] property`
       )
 
     const events = entity.newEvents
@@ -182,25 +153,19 @@ export class Repository {
         event[index] = entity[index]
       })
     })
-    this.events.insertMany(events, (err) => {
-      if (err) return cb(err)
-      log('committed %s.events for id %s', this.entityType.name, entity.id)
-      entity.newEvents = []
-      return cb()
-    })
+    await this.events.insertMany(events)
+    log('committed %s.events for id %s', this.entityType.name, entity.id)
+    entity.newEvents = []
   }
 
-  _commitAllEvents(entities, cb) {
+  async _commitAllEvents(entities: Entity[]): Promise<void> {
     const events = []
     entities.forEach((entity) => {
       if (entity.newEvents.length === 0) return
 
       if (!entity.id)
-        return cb(
-          new Error(
-            `Cannot commit an entity of type ${this.entityType} without an [id] property`
-            // self.entityType
-          )
+        throw new Error(
+          `Cannot commit an entity of type ${this.entityType} without an [id] property`
         )
 
       const evnts = entity.newEvents
@@ -213,52 +178,53 @@ export class Repository {
       Array.prototype.unshift.apply(events, evnts)
     })
 
-    if (events.length === 0) return cb()
+    if (events.length === 0) return
 
-    this.events.insertMany(events, (err) => {
-      if (err) return cb(err)
-      log(
-        'committed %s.events for ids %j',
-        this.entityType.name,
-        _.map(entities, 'id')
-      )
-      entities.forEach((entity) => {
-        entity.newEvents = []
-      })
-      return cb()
+    this.events.insertMany(events)
+    log(
+      'committed %s.events for ids %j',
+      this.entityType.name,
+      _.map(entities, 'id')
+    )
+    entities.forEach((entity) => {
+      entity.newEvents = []
     })
   }
 
-  _commitSnapshots(entity, options, cb) {
+  async _commitSnapshots(
+    entity: Entity,
+    options: { forceSnapshot: boolean }
+  ): Promise<any> {
     if (
       options.forceSnapshot ||
       entity.version >= entity.snapshotVersion + this.snapshotFrequency
     ) {
-      const snapshot = entity.snapshot()
+      const snapshot: Snapshot = entity.snapshot() as unknown as Snapshot
       if (snapshot && snapshot._id) delete snapshot._id // mongo will blow up if we try to insert multiple _id keys
-      this.snapshots.insertOne(snapshot, (err) => {
-        if (err) return cb(err)
-        log(
-          'committed %s.snapshot for id %s %j',
-          this.entityType.name,
-          entity.id,
-          snapshot
-        )
-        return cb(null, entity)
-      })
+      await this.snapshots.insertOne(snapshot)
+      log(
+        'committed %s.snapshot for id %s %j',
+        this.entityType.name,
+        entity.id,
+        snapshot
+      )
+      return entity
     } else {
-      return cb(null, entity)
+      return entity
     }
   }
 
-  _commitAllSnapshots(entities, options, cb) {
+  async _commitAllSnapshots(
+    entities: Entity[],
+    options: { forceSnapshot: boolean }
+  ): Promise<SourcedEntity[]> {
     const snapshots = []
     entities.forEach((entity) => {
       if (
         options.forceSnapshot ||
         entity.version >= entity.snapshotVersion + this.snapshotFrequency
       ) {
-        const snapshot = entity.snapshot()
+        const snapshot = entity.snapshot() as unknown as Snapshot
         if (snapshot) {
           if (snapshot._id) delete snapshot._id // mongo will blow up if we try to insert multiple _id keys)
           snapshots.push(snapshot)
@@ -266,28 +232,26 @@ export class Repository {
       }
     })
 
-    if (snapshots.length === 0) return cb()
+    if (snapshots.length === 0) return
 
-    this.snapshots.insertMany(snapshots, (err) => {
-      if (err) return cb(err)
-      log(
-        'committed %s.snapshot for ids %s %j',
-        this.entityType.name,
-        _.map(entities, 'id'),
-        snapshots
-      )
-      return cb(null, entities)
-    })
+    await this.snapshots.insertMany(snapshots)
+    log(
+      'committed %s.snapshot for ids %s %j',
+      this.entityType.name,
+      _.map(entities, 'id'),
+      snapshots
+    )
+    return entities
   }
 
-  _deserialize(id, snapshot, events) {
+  _deserialize(id, snapshot, events): SourcedEntity {
     log('deserializing %s entity ', this.entityType.name)
     const entity = new this.entityType(snapshot, events)
     entity.id = id
     return entity
   }
 
-  _emitEvents(entity) {
+  _emitEvents(entity: Entity): void {
     const eventsToEmit = entity.eventsToEmit
     entity.eventsToEmit = []
     eventsToEmit.forEach((eventToEmit) => {
@@ -298,41 +262,35 @@ export class Repository {
     log('emitted local events for id %s', entity.id)
   }
 
-  _getAllSnapshots(ids, cb) {
+  async _getAllSnapshots(ids: string[]): Promise<Snapshot[]> {
     const match = { $match: { id: { $in: ids } } }
     const sort = { $sort: { snapshotVersion: 1 } }
     const group = {
       $group: { _id: '$id', snapshotVersion: { $last: '$snapshotVersion' } }
     }
 
-    this.snapshots.aggregate([match, sort, group], (err, cursor) => {
-      if (err) return cb(err)
-      cursor.toArray((err, idVersionPairs) => {
-        if (err) return cb(err)
-        let criteria: { $or?: any; id?: any; snapshotVersion?: any } = {}
-        if (idVersionPairs.length === 0) {
-          return cb(null, [])
-        } else if (idVersionPairs.length === 1) {
-          criteria = {
-            id: idVersionPairs[0]._id,
-            snapshotVersion: idVersionPairs[0].snapshotVersion
-          }
-        } else {
-          criteria.$or = []
-          idVersionPairs.forEach((pair) => {
-            const cri = { id: pair._id, snapshotVersion: pair.snapshotVersion }
-            criteria.$or.push(cri)
-          })
-        }
-        this.snapshots.find(criteria).toArray((err, snapshots) => {
-          if (err) cb(err)
-          return cb(null, snapshots)
-        })
+    const cursor = this.snapshots.aggregate([match, sort, group])
+    const idVersionPairs = await cursor.toArray()
+    let criteria: { $or?: any; id?: any; snapshotVersion?: any } = {}
+    if (idVersionPairs.length === 0) {
+      return []
+    } else if (idVersionPairs.length === 1) {
+      criteria = {
+        id: idVersionPairs[0]._id,
+        snapshotVersion: idVersionPairs[0].snapshotVersion
+      }
+    } else {
+      criteria.$or = []
+      idVersionPairs.forEach((pair) => {
+        const cri = { id: pair._id, snapshotVersion: pair.snapshotVersion }
+        criteria.$or.push(cri)
       })
-    })
+    }
+    const snapshots = await this.snapshots.find(criteria).toArray()
+    return snapshots
   }
 
-  _getAllEvents(ids, snapshots, cb) {
+  async _getAllEvents(ids, snapshots): Promise<Entity[]> {
     const criteria = { $or: [] }
     ids.forEach((id) => {
       let snapshot
@@ -346,23 +304,19 @@ export class Repository {
       }
     })
 
-    this.events
+    const events = await this.events
       .find(criteria)
       .sort({ id: 1, version: 1 })
-      .toArray((err, events) => {
-        if (err) return cb(err)
-        if (!snapshots.length && !events.length) return cb(null, null)
-        const results = []
-        ids.forEach((id) => {
-          const snapshot = _.find(snapshots, (snapshot) => snapshot.id === id)
-          if (snapshot) delete snapshot._id
-          const evnts = _.filter(events, (event) => event.id === id)
-          const entity = this._deserialize(id, snapshot, evnts)
-          results.push(entity)
-        })
-        return cb(null, results)
-      })
+      .toArray()
+    if (!snapshots.length && !events.length) return
+    const results = []
+    ids.forEach((id) => {
+      const snapshot = _.find(snapshots, (snapshot) => snapshot.id === id)
+      if (snapshot) delete snapshot._id
+      const evnts = _.filter(events, (event) => event.id === id)
+      const entity = this._deserialize(id, snapshot, evnts)
+      results.push(entity)
+    })
+    return results
   }
 }
-
-// module.exports.Repository = Repository;
